983bc5398ea639ef61c3d251ddea8fbf1608e85e
Satakshi <77228717+sats500@users.noreply.github.com>
2023-12-16 00:36:24 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import {useState} from 'react';
+import './App.css';
+import LeftNav from './components/LeftNav/LeftNav';
+import MainSection from './components/MainSection/MainSection'; // Or adjust based on your file structure
+import TopNav from './components/TopNav/TopNav';
+
+
+function App() {
+  const [employeeId,setEmployeeId]=useState('')
+  console.log(employeeId)
+  return (
+    <div className="App">
+      <TopNav/>
+      <LeftNav employeeId={employeeId}/>
+      <MainSection setEmployeeId={setEmployeeId}/>
+    </div>
+  );
+}
+
+export default App;
